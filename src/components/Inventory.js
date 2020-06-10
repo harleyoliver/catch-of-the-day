f5ef1a8f984748358cdb89e7373422ac7efcd6c9
Harley Oliver <hello@heronamedharley.com>
2020-06-10 16:40:09 +1000

    Creating our application layout with components

---
+++ src/components/Inventory.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Inventory extends React.Component {
+    render() {
+        return(
+            <div className="inverntory">
+                
+            </div>
+        )
+    }
+}
+
+export default Inventory;
